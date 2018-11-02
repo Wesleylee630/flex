6581851c6a44b0ea84fea381ac295f807c46943f
Wesley <Wesley@Wesleys-MBP.gateway>
2018-11-02 16:30:23 +1100

    update

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function nav() {
+  var option = document.getElementById("list");
+  if (option.style.marginTop == "-245px"){
+    option.style.marginTop = "0";
+  } 
+  else{
+  	option.style.marginTop = "-245px";
+  }
+}
+
+nav();
